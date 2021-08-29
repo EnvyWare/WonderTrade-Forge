f3f121da0ae75c8e15e35d847e7977f7de682183
danorris709 <dnorris443@gmail.com>
2021-08-29 11:55:23 +0100

    feat(forge): add more config settings

--- forge/src/main/java/com/envyful/wonder/trade/forge/config/WonderTradeConfig.java
+++ forge/src/main/java/com/envyful/wonder/trade/forge/config/WonderTradeConfig.java
@@ -2,6 +2,7 @@ package com.envyful.wonder.trade.forge.config;
 
 import com.envyful.api.config.data.ConfigPath;
 import com.envyful.api.config.yaml.AbstractYamlConfig;
+import com.envyful.api.math.UtilRandom;
 import com.google.common.collect.Sets;
 import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import com.pixelmonmod.pixelmon.api.pokemon.PokemonSpec;
@@ -9,18 +10,28 @@ import com.pixelmonmod.pixelmon.enums.EnumSpecies;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 @ConfigPath("config/WonderTradeForge/config.yml")
 @ConfigSerializable
 public class WonderTradeConfig extends AbstractYamlConfig {
 
     private GenerationSettings defaultGeneratorSettings = new GenerationSettings();
+    private int cooldownSeconds = 3600;
+    private int minRequiredLevel = 30;
 
-    public WonderTradeConfig() {
-    }
+    public WonderTradeConfig() {}
 
     public GenerationSettings getDefaultGeneratorSettings() {
         return this.defaultGeneratorSettings;
+    }
+
+    public int getCooldownSeconds() {
+        return this.cooldownSeconds;
+    }
+
+    public int getMinRequiredLevel() {
+        return this.minRequiredLevel;
     }
 
     @ConfigSerializable
@@ -29,6 +40,9 @@ public class WonderTradeConfig extends AbstractYamlConfig {
         private Set<EnumSpecies> blockedTypes = Sets.newHashSet(EnumSpecies.Hoopa);
         private boolean allowLegends = true;
         private boolean allowUltraBeasts = true;
+        private double shinyChance = 0.05;
+        private int minLevel = 10;
+        private int maxLevel = 70;
 
         public GenerationSettings() {
         }
@@ -36,14 +50,18 @@ public class WonderTradeConfig extends AbstractYamlConfig {
         public Pokemon build() {
             EnumSpecies species = this.getRandomSpecies();
             PokemonSpec spec = new PokemonSpec();
+
             spec.name = species.name;
+            spec.shiny = ThreadLocalRandom.current().nextDouble() < this.shinyChance;
+            spec.level = UtilRandom.randomInteger(this.minLevel, this.maxLevel);
+
             return spec.create();
         }
 
         private EnumSpecies getRandomSpecies() {
             EnumSpecies species = EnumSpecies.randomPoke(this.allowLegends);
 
-            while (blockedTypes.contains(species) || (!this.allowLegends && species.isUltraBeast())) {
+            while (blockedTypes.contains(species) || (!this.allowUltraBeasts && species.isUltraBeast())) {
                 species = EnumSpecies.randomPoke(this.allowLegends);
             }
 
