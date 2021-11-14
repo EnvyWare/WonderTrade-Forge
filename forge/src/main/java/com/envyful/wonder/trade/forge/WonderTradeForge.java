70bdbc3a1cb04494afdc40c5bc1934aa33cd0890
Daniel Norris <dnorris443@gmail.com>
2021-11-14 22:22:20 +0000

    chore(gradle): bump version

--- forge/src/main/java/com/envyful/wonder/trade/forge/WonderTradeForge.java
+++ forge/src/main/java/com/envyful/wonder/trade/forge/WonderTradeForge.java
@@ -32,7 +32,7 @@ import java.sql.SQLException;
         acceptableRemoteVersions = "*"
 )
 public class WonderTradeForge {
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
 
     private static WonderTradeForge instance;
 
