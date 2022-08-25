dbd706a867158c8d134bb890498a80fb6fb6bdc7
Daniel Norris <dnorris443@gmail.com>
2022-08-25 17:19:01 +0100

    chore(gradle): bump API version

--- forge/src/main/java/com/envyful/wonder/trade/forge/WonderTradeForge.java
+++ forge/src/main/java/com/envyful/wonder/trade/forge/WonderTradeForge.java
@@ -33,7 +33,7 @@ import java.sql.SQLException;
         updateJSON = "https://ogn.pixelmonmod.com/update/sm-wt/update.json"
 )
 public class WonderTradeForge {
-    public static final String VERSION = "6.0.6";
+    public static final String VERSION = "6.0.7";
 
     private static WonderTradeForge instance;
 
