bf209eeef9b70778ac070ef400ceb1c991dd7a25
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2023-03-08 17:33:42 +0000

    chore(gradle): bump version to 6.4.2

--- forge/src/main/java/com/envyful/wonder/trade/forge/WonderTradeForge.java
+++ forge/src/main/java/com/envyful/wonder/trade/forge/WonderTradeForge.java
@@ -33,7 +33,7 @@ import java.sql.SQLException;
         updateJSON = "https://ogn.pixelmonmod.com/update/sm-wt/update.json"
 )
 public class WonderTradeForge {
-    public static final String VERSION = "6.4.1";
+    public static final String VERSION = "6.4.2";
 
     private static WonderTradeForge instance;
 
